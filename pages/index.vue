9beebf220d0f8b34f2c746d96b63492b6aec15cd
Vincent Woltmann <vincent.woltmann@protonmail.com>
2020-08-05 23:33:21 +0200

    First Try at generating pdf

--- pages/index.vue
+++ pages/index.vue
@@ -4,49 +4,49 @@
       <b-step-item step="1" label="Veranstalter*in">
         <section class="section">
           <b-field label="Name">
-            <b-input />
+            <b-input v-model="veranstalterName" />
           </b-field>
           <b-field label="Straße & Hausnummer" expanded>
-            <b-input />
+            <b-input v-model="veranstalterStreet" />
           </b-field>
           <b-field label="Adresszusatz" expanded>
-            <b-input />
+            <b-input v-model="veranstalterAdressZusatz" />
           </b-field>
           <b-field horizontal expanded custom-class="is-hidden">
-            <b-field label="Plz." expanded>
-              <b-input />
+            <b-field label="Postleitzahl" expanded>
+              <b-input v-model="veranstalterPLZ" />
             </b-field>
             <b-field label="Ort" expanded>
-              <b-input />
+              <b-input v-model="veranstalterOrt" />
             </b-field>
           </b-field>
         </section>
       </b-step-item>
-      <b-step-item step="2" label="Demoleitung/Veranstalter*in">
+      <b-step-item step="2" label="Versammlungsleitung">
         <b-field label="Name">
-          <b-input />
+          <b-input v-model="versammlungsLeitungName" />
         </b-field>
         <b-field label="Telefonnummer/Faxnummer">
-          <b-input />
+          <b-input v-model="versammlungsLeitungNummer" />
         </b-field>
         <b-field label="Straße & Hausnummer" expanded>
-          <b-input />
+          <b-input v-model="versammlungsLeitungStraße" />
         </b-field>
         <b-field label="Adresszusatz" expanded>
-          <b-input />
+          <b-input v-model="versammlungsLeitungAdresszusatz" />
         </b-field>
         <b-field horizontal expanded custom-class="is-hidden">
-          <b-field label="Plz." expanded>
-            <b-input />
+          <b-field label="Postleitzahl" expanded>
+            <b-input v-model="versammlungsLeitungPLZ" />
           </b-field>
           <b-field label="Ort" expanded>
-            <b-input />
+            <b-input v-model="versammlungsLeitungOrt" />
           </b-field>
         </b-field>
       </b-step-item>
       <b-step-item step="3" label="Zeitlicher Ablauf">
         <b-field label="Tag der Veranstaltung">
-          <b-datepicker> </b-datepicker>
+          <b-datepicker v-model="date"> </b-datepicker>
         </b-field>
         <b-field label="Voraussichtlicher Start">
           <b-timepicker> </b-timepicker>
@@ -95,11 +95,88 @@
         </b-field>
       </b-step-item>
     </b-steps>
+    <b-button @click="createPDF">
+      Download
+    </b-button>
   </form>
 </template>
 
-<script>
-export default {
-  name: 'HomePage',
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+import moment from 'moment'
+import pdfMake from 'pdfmake/build/pdfmake'
+import pdfFonts from 'pdfmake/build/vfs_fonts'
+
+pdfMake.vfs = pdfFonts.pdfMake.vfs
+
+@Component
+export default class IndexView extends Vue {
+  date = new Date()
+  veranstalterName = ''
+  veranstalterAdressZusatz = ''
+  veranstalterStreet = ''
+  veranstalterPLZ = ''
+  veranstalterOrt = ''
+
+  versammlungsLeitungName = ''
+  versammlungsLeitungNummer = ''
+  versammlungsLeitungAdresszusatz = ''
+  versammlungsLeitungStraße = ''
+  versammlungsLeitungPLZ = ''
+  versammlungsLeitungOrt = ''
+
+  createPDF() {
+    const demonstrationDate = moment(this.date).format('DD.MM.yyyy')
+    const docDefinition = {
+      content: [
+        `Sehr geehrte Mitarbeiter*innen der Versammlungsbehörde,
+
+Wir wollen für den ${demonstrationDate} eine Demo anmelden.
+
+Die benötigten Informationen für die Anmeldung der Demonstration und Kundgebung am ${demonstrationDate}:`,
+        {
+          table: {
+            body: [
+              ['Name des*r Veranstalter*in', this.veranstalterName],
+              [
+                'Anschrift des*r Veranstalter*in',
+                (this.veranstalterAdressZusatz
+                  ? this.veranstalterAdressZusatz + '\n'
+                  : '') +
+                  this.veranstalterStreet +
+                  '\n' +
+                  this.veranstalterPLZ +
+                  ' ' +
+                  this.veranstalterOrt,
+              ],
+              ['Name der Versammlungsleitung', this.versammlungsLeitungName],
+              [
+                'Anschrift der Versammlungsleitung',
+                (this.versammlungsLeitungAdresszusatz
+                  ? this.versammlungsLeitungAdresszusatz + '\n'
+                  : '') +
+                  this.versammlungsLeitungStraße +
+                  '\n' +
+                  this.veranstalterPLZ +
+                  ' ' +
+                  this.versammlungsLeitungOrt,
+              ],
+              [
+                'Telefonnummer der Versammlungsleitung',
+                this.versammlungsLeitungNummer,
+              ],
+            ],
+          },
+        },
+        `
+Um Unklarheiten zu klären kommen wir auch gerne vorbei.
+
+Mit freundlichen Grüßen,
+
+${this.veranstalterName}`,
+      ],
+    }
+    pdfMake.createPdf(docDefinition).download('demo_anmeldung.pdf')
+  }
 }
 </script>
