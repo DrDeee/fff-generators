a9b6914539cb15f25cd1827efe45f5d0ea2ea43b
Vincent Woltmann <vincent.woltmann@protonmail.com>
2020-08-05 16:46:16 +0200

    Added mockup for hessen

--- pages/index.vue
+++ pages/index.vue
@@ -1,46 +1,100 @@
 <template>
-  <section class="section">
-    <div class="columns is-mobile">
-      <card title="Free" icon="github">
-        Open source on
-        <a href="https://github.com/buefy/buefy">
-          GitHub
-        </a>
-      </card>
-
-      <card title="Responsive" icon="cellphone-link">
-        <b class="has-text-grey">
-          Every
-        </b>
-        component is responsive
-      </card>
-
-      <card title="Modern" icon="alert-decagram">
-        Built with
-        <a href="https://vuejs.org/">
-          Vue.js
-        </a>
-        and
-        <a href="http://bulma.io/">
-          Bulma
-        </a>
-      </card>
-
-      <card title="Lightweight" icon="arrange-bring-to-front">
-        No other internal dependency
-      </card>
-    </div>
-  </section>
+  <form class="form">
+    <b-steps>
+      <b-step-item step="1" label="Veranstalter*in">
+        <section class="section">
+          <b-field label="Name">
+            <b-input />
+          </b-field>
+          <div class="box">
+            <b-field label="Straße & Hausnummer" expanded>
+              <b-input />
+            </b-field>
+            <b-field label="Adresszusatz" expanded>
+              <b-input />
+            </b-field>
+            <b-field grouped>
+              <b-field label="Plz.">
+                <b-input expanded />
+              </b-field>
+              <b-field label="Ort" expanded>
+                <b-input />
+              </b-field>
+            </b-field>
+          </div>
+        </section>
+      </b-step-item>
+      <b-step-item step="2" label="Demoleitung/Veranstalter*in">
+        <b-field label="Name">
+          <b-input />
+        </b-field>
+        <b-field label="Telefonnummer/Faxnummer">
+          <b-input />
+        </b-field>
+        <div class="box">
+          <b-field label="Straße & Hausnummer" expanded>
+            <b-input />
+          </b-field>
+          <b-field label="Adresszusatz" expanded>
+            <b-input />
+          </b-field>
+          <b-field grouped>
+            <b-field label="Plz.">
+              <b-input expanded />
+            </b-field>
+            <b-field label="Ort" expanded>
+              <b-input />
+            </b-field>
+          </b-field>
+        </div>
+      </b-step-item>
+      <b-step-item step="3" label="Zeitlicher Ablauf">
+        <b-field label="Tag der Veranstaltung">
+          <b-datepicker> </b-datepicker>
+        </b-field>
+        <b-field label="Voraussichtlicher Start">
+          <b-timepicker> </b-timepicker>
+        </b-field>
+        <b-field label="Voraussichtliches Ende">
+          <b-timepicker> </b-timepicker>
+        </b-field>
+        <div class="box">
+          <b-field label="Startpunkt">
+            <b-input />
+          </b-field>
+          <b-field label="Route">
+            <b-input type="textarea" />
+          </b-field>
+        </div>
+      </b-step-item>
+      <b-step-item step="4" label="Ablauf">
+        <b-field label="Art und Gegenstand der Versammlung">
+          <b-input />
+        </b-field>
+        <b-field label="Ablauf der Versammlung">
+          <b-input type="textarea" />
+        </b-field>
+      </b-step-item>
+      <b-step-item step="4" label="Details">
+        <b-field label="Voraussichtliche Teilnehmer*innen">
+          <b-input type="number" />
+        </b-field>
+        <b-field label="Anzahl Ordner*innen">
+          <b-input type="number" />
+        </b-field>
+        <b-field
+          label="Vorgesehene Hilfsmittel (Lautsprecher, Transparente,
+          etc.)"
+        >
+          <b-input type="textarea" />
+        </b-field>
+      </b-step-item>
+    </b-steps>
+  </form>
 </template>
 
 <script>
-import Card from '~/components/Card'
-
 export default {
   name: 'HomePage',
-
-  components: {
-    Card,
-  },
 }
 </script>
