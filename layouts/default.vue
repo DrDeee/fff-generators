2a57fdeb12331160fdaad09796c444bf70425396
Vincent Woltmann <vincent.woltmann@protonmail.com>
2020-09-02 22:54:59 +0200

    Removed the unused sidebar from the layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -18,24 +18,9 @@
       </div>
     </nav>
 
-    <section class="main-content columns">
-      <aside class="column is-2 section">
-        <p class="menu-label is-hidden-touch">
-          General
-        </p>
-        <ul class="menu-list">
-          <li v-for="(item, key) of items" :key="key">
-            <nuxt-link :to="item.to" exact-active-class="is-active">
-              <b-icon :icon="item.icon" /> {{ item.title }}
-            </nuxt-link>
-          </li>
-        </ul>
-      </aside>
-
-      <div class="container column is-10">
-        <nuxt />
-      </div>
-    </section>
+    <div class="container column">
+      <nuxt />
+    </div>
   </div>
 </template>
 
