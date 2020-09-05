5c88627284e5d1d47119b087771274998688cbb6
Jonas <Jones132@posteo.net>
2020-09-05 18:11:53 +0200

    remove script which (I assume) is unnecessary now

--- layouts/default.vue
+++ layouts/default.vue
@@ -17,24 +17,3 @@
     </div>
   </div>
 </template>
-
-<script>
-export default {
-  data() {
-    return {
-      items: [
-        {
-          title: 'Home',
-          icon: 'home',
-          to: { name: 'index' },
-        },
-        {
-          title: 'Inspire',
-          icon: 'lightbulb',
-          to: { name: 'inspire' },
-        },
-      ],
-    }
-  },
-}
-</script>
