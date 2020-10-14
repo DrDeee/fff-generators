d460f2be428d4cb654a5050dc3c8be0722a67e83
Jonas (Karlsruhe) <Jonas13@posteo.de>
2020-10-14 20:56:35 +0200

    Create new empty page and bring back the sidebar

--- layouts/default.vue
+++ layouts/default.vue
@@ -14,9 +14,22 @@
     </nav>
     -->
 
-    <div class="container column">
-      <nuxt />
-    </div>
+    <section class="main-content columns">
+      <aside class="column is-2 section">
+        <p class="menu-label is-hidden-touch">General</p>
+        <ul class="menu-list">
+          <li v-for="(item, key) of items" :key="key">
+            <nuxt-link :to="item.to" exact-active-class="is-active">
+              <b-icon :icon="item.icon" /> {{ item.title }}
+            </nuxt-link>
+          </li>
+        </ul>
+      </aside>
+
+      <div class="container column is-10">
+        <nuxt />
+      </div>
+    </section>
 
     <footer class="footer">
       <div class="has-text-centered">
@@ -25,3 +38,24 @@
     </footer>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          title: 'Demoanmeldung',
+          icon: '',
+          to: { name: 'index' },
+        },
+        {
+          title: 'Pressemitteilungen',
+          icon: '',
+          to: { name: 'press_release' },
+        },
+      ],
+    }
+  },
+}
+</script>
